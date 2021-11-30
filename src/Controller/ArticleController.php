a5a6f09f4545db1f9be7856e5582b733a9fbc351
laure.lenouail <laure.lenouail@gmail.com>
2021-11-30 14:50:10 +0000

    suite tp1

--- src/Controller/ArticleController.php
+++ src/Controller/ArticleController.php
@@ -5,16 +5,49 @@ namespace App\Controller;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
+use App\Entity\Article;
+use App\Repository\ArticleRepository;
 
 class ArticleController extends AbstractController
 {
     /**
      * @Route("/article", name="article.index")
      */
-    public function index(): Response
+    public function index(ArticleRepository $articleRepository): Response
     {
+       
+        
+        $articles = $articleRepository->findAll();
+
+
+
+
         return $this->render('article/index.html.twig', [
             'controller_name' => 'ArticleController',
+            'articles'=> $articles,
+        ]);
+    }
+
+
+    /**
+     * @Route("/article/{id}", name="article.show")
+     */
+
+    public function show($id): Response{
+
+        $articleRepository = $this->getDoctrine()->getRepository(Article::class);
+        
+        $article = $articleRepository->find($id);
+
+        if (!$article)
+            {
+                throw $this->createNotFoundException('The article does not exist');
+            }
+
+
+        return $this->render('article/show.html.twig', [
+            'controller_name' => 'ArticleController',
+            'article'=> $article,
         ]);
     }
 }
---
+++ src/DataFixtures/ArticleFixtures.php
@@ -0,0 +1,33 @@
+<?php
+
+namespace App\DataFixtures;
+
+use Doctrine\Bundle\FixturesBundle\Fixture;
+use Doctrine\Persistence\ObjectManager;
+use Faker;
+use App\Entity\Article;
+use Faker\Factory;
+
+class ArticleFixtures extends Fixture
+{
+    public function load(ObjectManager $manager)
+{
+    $faker = Factory::create();
+
+    for ($i = 1; $i <= 10; $i++)
+    {
+        $article = new Article();
+
+        $sentence = $faker->sentence(4);
+        $title = substr($sentence, 0, strlen($sentence) - 1);
+        $article->setTitle($title)
+                ->setAuthor($faker->name())
+                ->setContent($faker->text(3000))
+                ->setCreatedAt($faker->dateTimeThisYear());
+
+        $manager->persist($article);
+    }
+
+    $manager->flush();
+}
+}
